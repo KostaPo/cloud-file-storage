b1808fada5899b9d16124e6482e376ba958413d1
KostaPo <kosta.po@mail.ru>
2024-01-27 23:05:39 +0300

    add logger (Log4j2)

--- src/main/java/ru/kostapo/cloudfilestorage/MainController.java
+++ src/main/java/ru/kostapo/cloudfilestorage/MainController.java
@@ -1,15 +1,19 @@
 package ru.kostapo.cloudfilestorage;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Log4j2
 @Controller
 public class MainController {
+
 
     @RequestMapping({"", "/"})
     @ResponseBody
     public String index() {
+        log.info("info");
         return "Hello";
     }
 }
